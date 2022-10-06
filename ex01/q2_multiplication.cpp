5b1c7430731e047865eedcca66f1ff2f78cfdcb4
id-isymhrk <isymhrk0608@gmail.com>
2022-10-06 12:20:23 +0900

    q2用ファイル追加

---
+++ ex01/q2_multiplication.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+#include <climits>
+
+using namespace std;
+
+vector<unsigned long> test1 = {1};
+vector<unsigned long> test2 = {1};
+
+unsigned long Multiplication(unsigned long a, unsigned long b)
+{
+}
+
+int main()
+{
+    return 0;
+}
