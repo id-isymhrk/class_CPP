1b3aab637053192e9dae595c76d6bc7c41b95b34
id-isymhrk <isymhrk0608@gmail.com>
2022-10-06 11:50:30 +0900

    q1完成

--- ex01/q1_parity.cpp
+++ ex01/q1_parity.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <bitset>
+#include <climits>
 
 using namespace std;
 
-vector<unsigned long> test = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+vector<unsigned long> test = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 105, 44, 64};
 
 int parity(unsigned long val)
 {
@@ -24,6 +25,7 @@ int main()
     {
         cout << "input:" << test[i] << endl;
         cout << "input:" << bitset<8>(test[i]) << endl;
+        cout << "input:" << bitset<sizeof(long) * CHAR_BIT>(test[i]) << endl;
         cout << "result:" << parity(test[i]) << endl;
         cout << endl;
     }
