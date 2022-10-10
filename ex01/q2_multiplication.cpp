18c41535e1002ad95ce97d03931c37c5a21ac1c0
id-isymhrk <isymhrk0608@gmail.com>
2022-10-11 03:18:58 +0900

    念のため計算方法を１つ追加した

--- ex01/q2_multiplication.cpp
+++ ex01/q2_multiplication.cpp
@@ -25,8 +25,9 @@ void Multiplication(unsigned long a1, unsigned long b1)
             for (int j = 0; j < sizeof(unsigned long); j++)
             {
                 if (b1 & 1 << j)
+                {
                     calc[j + i]++; //桁をずらして加算していく
-
+                }
                 // cout << "test:calc[" << j + i << "]=" << calc[j + i] << endl;
             }
         }
@@ -37,7 +38,7 @@ void Multiplication(unsigned long a1, unsigned long b1)
     }
 
     // output
-    cout << "answer(bit):";
+    cout << "answer(vector) :";
     for (int i = calc.size() - 1; i >= 0; i--)
     {
         cout << calc[i];
@@ -45,23 +46,71 @@ void Multiplication(unsigned long a1, unsigned long b1)
     cout << endl;
 }
 
+void Add(unsigned long &a2, int digit)
+{
+    int tmp = a2;
+    if (tmp >> digit & 1) //該当の桁が既に１かどうかの判定
+    {
+        //桁上げ
+        a2 ^= 1 << digit;
+        Add(a2, digit + 1);
+    }
+    else
+    {
+        //普通に足し算
+        a2 |= 1 << digit;
+        // cout << "Add:digit " << digit;
+        // cout << "   Add:a2 " << bitset<sizeof(unsigned long)>(a2) << endl;
+    }
+}
+
+void Multiplication2(unsigned long a1, unsigned long b1)
+{
+    unsigned long ans = 0, tmp = 0;
+
+    // bit calc
+    for (int i = 0; i < sizeof(unsigned long); i++)
+    {
+
+        if (a1 & 1 << i)
+        {
+            tmp = ans;
+            for (int j = 0; j < sizeof(unsigned long); j++)
+            {
+                if (b1 & 1 << j)
+                {
+                    // cout << "i=" << i << " j=" << j << "  Call Add" << endl;
+                    Add(ans, i + j);
+                }
+            }
+        }
+    }
+
+    // output
+    cout << "answer(bit)    :";
+    cout << bitset<sizeof(unsigned long) * CHAR_BIT>(ans);
+    cout << endl;
+}
+
 int main()
 {
-    vector<unsigned long> test1 = {5, 10, 12, 33, 51, 2, 100};
-    vector<unsigned long> test2 = {4, 11, 6, 3, 20, 4, 8};
+    // vector<unsigned long> test1 = {1, 3};
+    // vector<unsigned long> test2 = {1, 2};
+    vector<unsigned long> test1 = {1, 5, 10, 12, 33, 51, 2, 100};
+    vector<unsigned long> test2 = {1, 4, 11, 6, 3, 20, 4, 8};
     const int LOOP_NUM = test1.size() > test2.size() ? test1.size() : test2.size();
 
     for (int i = 0; i < LOOP_NUM; i++)
     {
         cout << "----------" << endl;
-        cout << "test1:" << test1[i] << endl;
-        // cout << "test1(bit):" << bitset<sizeof(unsigned long)>(test1[i]) << endl;
-        cout << "test2:" << test2[i] << endl;
-        // cout << "test2(bit):" << bitset<sizeof(unsigned long)>(test2[i]) << endl;
+        // cout << "test1:" << test1[i] << endl;
+        cout << "test1(bit):" << bitset<sizeof(unsigned long)>(test1[i]) << endl;
+        // cout << "test2:" << test2[i] << endl;
+        cout << "test2(bit):" << bitset<sizeof(unsigned long)>(test2[i]) << endl;
         cout << "answer:" << test1[i] * test2[i] << endl;
-        cout << "answer(bit):" << bitset<sizeof(unsigned long) * CHAR_BIT>(test1[i] * test2[i]) << endl;
-        // cout << "answer(bit):" << Multiplication(test1[i], test2[i]) << endl;
+        cout << "answer(default):" << bitset<sizeof(unsigned long) * CHAR_BIT>(test1[i] * test2[i]) << endl;
         Multiplication(test1[i], test2[i]);
+        Multiplication2(test1[i], test2[i]);
     }
 
     return 0;
