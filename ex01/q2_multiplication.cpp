0c3f0c18fa7ff9510bf87af97a11deb16c003281
id-isymhrk <isymhrk0608@gmail.com>
2022-10-11 02:09:15 +0900

    計算方法が謝っていたので修正

--- ex01/q2_multiplication.cpp
+++ ex01/q2_multiplication.cpp
@@ -5,17 +5,59 @@
 
 using namespace std;
 
-bitset<sizeof(unsigned long)> Multiplication(unsigned long a1, unsigned long b1)
+// bitset<sizeof(unsigned long)> Multiplication(unsigned long a1, unsigned long b1)
+// {
+//     // unsigned long ans;
+//     // bitset<sizeof(unsigned long)> a2 = bitset<sizeof(unsigned long)>(a1);
+//     // bitset<sizeof(unsigned long)> b2 = bitset<sizeof(unsigned long)>(b1);
+//     // unsigned long divide = sizeof(unsigned long);
+
+//     bitset<sizeof(unsigned long)> ans = 0x00000000;
+//     ans = bitset<sizeof(unsigned long)>(a1 * b1);
+
+//     return ans;
+// }
+
+void Multiplication(unsigned long a1, unsigned long b1)
 {
-    // unsigned long ans;
-    // bitset<sizeof(unsigned long)> a2 = bitset<sizeof(unsigned long)>(a1);
-    // bitset<sizeof(unsigned long)> b2 = bitset<sizeof(unsigned long)>(b1);
-    // unsigned long divide = sizeof(unsigned long);
 
-    bitset<sizeof(unsigned long)> ans = 0x00000000;
-    ans = bitset<sizeof(unsigned long)>(a1 * b1);
+    // vector<vector<int>> calc;
+    vector<int> calc;
 
-    return ans;
+    // init
+    calc.resize(sizeof(unsigned long) * CHAR_BIT);
+    for (int i = 0; i < calc.size(); i++)
+    {
+        calc[i] = 0;
+    }
+
+    // bit calc
+    for (int i = 0; i < sizeof(unsigned long); i++)
+    {
+        if (a1 & 1 << i)
+        {
+            for (int j = 0; j < sizeof(unsigned long); j++)
+            {
+                // calc[i][j] += (b1 & 1 << j);
+                if (b1 & 1 << j)
+                    calc[j + i]++;
+                // cout << "test:calc[" << j + i << "]=" << calc[j + i] << endl;
+            }
+
+            // calc[i + 1][j] = calc[i][j]
+        }
+
+        calc[i + 1] += calc[i] / 2;
+        calc[i] = calc[i] % 2;
+    }
+
+    // output
+    cout << "answer(bit):";
+    for (int i = calc.size() - 1; i >= 0; i--)
+    {
+        cout << calc[i];
+    }
+    cout << endl;
 }
 
 int main()
@@ -26,9 +68,15 @@ int main()
 
     for (int i = 0; i < LOOP_NUM; i++)
     {
+        cout << "----------" << endl;
+        cout << "test1:" << test1[i] << endl;
+        // cout << "test1(bit):" << bitset<sizeof(unsigned long)>(test1[i]) << endl;
+        cout << "test2:" << test2[i] << endl;
+        // cout << "test2(bit):" << bitset<sizeof(unsigned long)>(test2[i]) << endl;
         cout << "answer:" << test1[i] * test2[i] << endl;
-        // cout << "answer(bit):" << bitset<sizeof(unsigned long)>(test1[i] * test2[i]) << endl;
-        cout << "answer(bit):" << Multiplication(test1[i], test2[i]) << endl;
+        cout << "answer(bit):" << bitset<sizeof(unsigned long) * CHAR_BIT>(test1[i] * test2[i]) << endl;
+        // cout << "answer(bit):" << Multiplication(test1[i], test2[i]) << endl;
+        Multiplication(test1[i], test2[i]);
     }
 
     return 0;
