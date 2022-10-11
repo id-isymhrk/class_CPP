3aa8cbf99d96191bb3166b8bfa970633a1288696
id-isymhrk <isymhrk0608@gmail.com>
2022-10-11 11:58:48 +0900

    実際の出力及び処理が完成

--- ex02/test_stack.cpp
+++ ex02/test_stack.cpp
@@ -1,7 +1,37 @@
 #include "stack.h"
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
+    // COMPLETE
+    // Write tests for testing your Stack implementation.
+    const int max_size = 10;
+    Point test_point[max_size];
+    Stack test_stack(max_size);
+
+    // init
+    for (int i = 0; i < max_size; i++)
+    {
+        test_point[i] = Point{i, i * 2};
+
+        if (!test_stack.full())
+        {
+            test_stack.push(test_point[i]);
+        }
+    }
+
+    for (int i = max_size - 1; i >= 0; i--)
+    {
+        cout << "----------" << endl;
+        cout << " test_point: " << test_point[i].x << ", " << test_point[i].y << endl;
+        cout << " test_stack: " << test_stack.top().x << ", " << test_stack.top().y << endl;
+        if (!test_stack.empty())
+        {
+            test_stack.pop();
+        }
+    }
 
     return 0;
 }
