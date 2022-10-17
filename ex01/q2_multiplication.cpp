ffaea237c8b01e482a794b56d844422ad0273093
id-isymhrk <isymhrk0608@gmail.com>
2022-10-17 11:41:44 +0900

    Q2:今度こそ算術演算子を使わないで実装できた

--- ex01/q2_multiplication.cpp
+++ ex01/q2_multiplication.cpp
@@ -7,46 +7,46 @@ using namespace std;
 
 const int MAX_BIT = 64;
 
-void Multiplication(unsigned long a1, unsigned long b1)
-{
-    vector<int> calc;
+// void Multiplication(unsigned long a1, unsigned long b1)
+// {
+//     vector<int> calc;
 
-    // init
-    calc.resize(sizeof(unsigned long) * CHAR_BIT);
-    for (int i = 0; i < calc.size(); i++)
-    {
-        calc[i] = 0;
-    }
+//     // init
+//     calc.resize(sizeof(unsigned long) * CHAR_BIT);
+//     for (int i = 0; i < calc.size(); i++)
+//     {
+//         calc[i] = 0;
+//     }
 
-    // bit calc
-    for (int i = 0; i < sizeof(unsigned long); i++)
-    {
+//     // bit calc
+//     for (int i = 0; i < sizeof(unsigned long); i++)
+//     {
 
-        if (a1 & 1 << i)
-        {
-            for (int j = 0; j < sizeof(unsigned long); j++)
-            {
-                if (b1 & 1 << j)
-                {
-                    calc[j + i]++; //桁をずらして加算していく
-                }
-                // cout << "test:calc[" << j + i << "]=" << calc[j + i] << endl;
-            }
-        }
+//         if (a1 & 1 << i)
+//         {
+//             for (int j = 0; j < sizeof(unsigned long); j++)
+//             {
+//                 if (b1 & 1 << j)
+//                 {
+//                     calc[j + i]++; //桁をずらして加算していく
+//                 }
+//                 // cout << "test:calc[" << j + i << "]=" << calc[j + i] << endl;
+//             }
+//         }
 
-        //桁上げの処理
-        calc[i + 1] += calc[i] / 2;
-        calc[i] = calc[i] % 2;
-    }
+//         //桁上げの処理
+//         calc[i + 1] += calc[i] / 2;
+//         calc[i] = calc[i] % 2;
+//     }
 
-    // output
-    cout << "answer(vector) :";
-    for (int i = calc.size() - 1; i >= 0; i--)
-    {
-        cout << calc[i];
-    }
-    cout << endl;
-}
+//     // output
+//     cout << "answer(vector) :";
+//     for (int i = calc.size() - 1; i >= 0; i--)
+//     {
+//         cout << calc[i];
+//     }
+//     cout << endl;
+// }
 
 void Add(unsigned long &a2, int digit_)
 {
@@ -73,24 +73,31 @@ void Add(unsigned long &a2, int digit_)
 void Multiplication2(unsigned long a1, unsigned long b1)
 {
     unsigned long ans = 0;
-    unsigned long tmp_a, tmp_b;
+    unsigned long tmp_a, tmp_b, tmp_i;
     // bit calc
     for (int i = 0; i < MAX_BIT; i++)
     {
         tmp_a = a1;
 
         // if (a1 & 1 << i)
-        if (tmp_a >> i & 1)
+        if (tmp_a >> i & 1) // aの２進数上の桁全てから１を判定
         {
-            // tmp = ans;
             for (int j = 0; j < MAX_BIT; j++)
             {
                 tmp_b = b1;
                 // if (b1 & 1 << j)
-                if (tmp_b >> j & 1)
+                if (tmp_b >> j & 1) //２進数上のbの全ての桁で１を判定。１だった桁の加算を行う
                 {
                     // cout << "i=" << i << " j=" << j << "  Call Add" << endl;
-                    Add(ans, i + j);
+                    // Add(ans, i + j);
+
+                    tmp_i = i;
+                    for (int k = 0; k < j; k++)
+                    {
+                        Add(tmp_i, 0); // tmp_iをj回加算する → imp_i=i+j
+                    }
+
+                    Add(ans, tmp_i); // tmp_i(i+j)の桁に１を足す
                 }
             }
         }
