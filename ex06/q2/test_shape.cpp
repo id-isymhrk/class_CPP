4d45239b145ef576a66aa5dc280d18090046c43a
id-isymhrk <isymhrk0608@gmail.com>
2022-10-27 02:05:55 +0900

    q2:基本的なテストは完了

--- ex06/q2/test_shape.cpp
+++ ex06/q2/test_shape.cpp
@@ -1,2 +1,25 @@
 #include "rectangle.h"
 #include "disk.h"
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    vector<Shape *> test;
+
+    cout << "-----input-----" << endl;
+    test.push_back(new Disk());
+    test.push_back(new Disk(Point(0, 0), 2));
+    test.push_back(new Rectangle());
+    test.push_back(new Rectangle(Point(0, 0), 2, 2));
+    cout << "-----result-----" << endl;
+    for (size_t i = 0; i < test.size(); i++)
+    {
+        cout << "Name:" << test[i]->get_name() << endl;
+        cout << "Area:" << test[i]->compute_area() << endl;
+    }
+    return 0;
+}
