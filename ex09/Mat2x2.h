54b8bd5616e4991f86706dcd3c15ab8b46acb639
id-isymhrk <isymhrk0608@gmail.com>
2022-11-09 23:38:29 +0900

    ファイル追加

---
+++ ex09/Mat2x2.h
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class Mat2x2
+{
+protected:
+    const size_t SIZE_COLIMN = 2;
+
+    float **_value;
+
+public:
+    // constructor
+    Mat2x2();
+    Mat2x2(float *array);
+    // copy
+    Mat2x2(const Mat2x2 &mat);
+    Mat2x2 &operator=(const Mat2x2 &mat);
+
+    Mat2x2 operator+(const Mat2x2 &mat);
+    Mat2x2 operator-(const Mat2x2 &mat);
+    Mat2x2 operator*(const Mat2x2 &mat);
+    Mat2x2 &operator+=(const Mat2x2 &mat);
+    Mat2x2 &operator-=(const Mat2x2 &mat);
+    Mat2x2 &operator*=(const Mat2x2 &mat);
+
+    float operator()(const int &i, const int &j);
+
+    bool operator==(const Mat2x2 &mat);
+
+    friend ostream &operator<<(ostream &os, const Mat2x2 &mat);
+
+    ~Mat2x2();
+};
---
+++ ex09/test_Mat2x2.cpp
@@ -0,0 +1,52 @@
+#include "Mat2x2.h"
+
+int main()
+{
+    Mat2x2 origin = Mat2x2();
+    // float *test_array = new float[4];
+    float array[] = {1.0, 1.0, 1.0, 1.0};
+    Mat2x2 test_mat = Mat2x2(array);
+
+    cout << "origin" << endl;
+    cout << origin << endl;
+    cout << "test_mat" << endl;
+    cout << test_mat << endl;
+    cout << "origin + test" << endl;
+    cout << origin + test_mat << endl;
+    cout << "origin - test" << endl;
+    cout << origin - test_mat << endl;
+    cout << "origin * test" << endl;
+    cout << origin * test_mat << endl;
+
+    Mat2x2 tmp = Mat2x2();
+    cout << "operator+=" << endl;
+    tmp += test_mat;
+    cout << tmp << endl;
+
+    Mat2x2 tmp2 = Mat2x2();
+    cout << "operator-=" << endl;
+    tmp2 -= test_mat;
+    cout << tmp2 << endl;
+
+    Mat2x2 tmp3 = Mat2x2();
+    cout << "operator*=" << endl;
+    tmp3 *= test_mat;
+    cout << tmp3 << endl;
+
+    cout << "operator==" << endl;
+    if (tmp3 == (origin * test_mat))
+    {
+        cout << "true" << endl;
+    }
+    else
+    {
+        cout << "false" << endl;
+    }
+    cout << endl;
+
+    cout << "origin*=origin" << endl;
+    origin *= origin;
+    cout << origin << endl;
+
+    return 0;
+}
