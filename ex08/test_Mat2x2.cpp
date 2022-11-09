5d329910fe6e819ce3bf4f65d23eb4ee454a4dcd
id-isymhrk <isymhrk0608@gmail.com>
2022-11-09 19:47:44 +0900

    テストファイル記述＋完成

--- ex08/test_Mat2x2.cpp
+++ ex08/test_Mat2x2.cpp
@@ -6,7 +6,6 @@ int main()
     // float *test_array = new float[4];
     float array[] = {1.0, 1.0, 1.0, 1.0};
     Mat2x2 test_mat = Mat2x2(array);
-    Mat2x2 tmp;
 
     cout << "origin" << endl;
     cout << origin << endl;
@@ -18,6 +17,36 @@ int main()
     cout << origin - test_mat << endl;
     cout << "origin * test" << endl;
     cout << origin * test_mat << endl;
+
+    Mat2x2 tmp = Mat2x2();
+    cout << "operator+=" << endl;
+    tmp += test_mat;
+    cout << tmp << endl;
+
+    Mat2x2 tmp2 = Mat2x2();
+    cout << "operator-=" << endl;
+    tmp2 -= test_mat;
+    cout << tmp2 << endl;
+
+    Mat2x2 tmp3 = Mat2x2();
+    cout << "operator*=" << endl;
+    tmp3 *= test_mat;
+    cout << tmp3 << endl;
+
+    cout << "operator==" << endl;
+    if (tmp3 == (origin * test_mat))
+    {
+        cout << "true" << endl;
+    }
+    else
+    {
+        cout << "false" << endl;
+    }
+    cout << endl;
+
+    cout << "origin*=origin" << endl;
+    origin *= origin;
+    cout << origin << endl;
 
     return 0;
 }
