ae58a54a0ca9484ec76e15e753c6e34e83ebbcf6
id-isymhrk <isymhrk0608@gmail.com>
2022-11-13 23:03:25 +0900

    ex10/テスト機能実装

--- ex10/test_ex10.cpp
+++ ex10/test_ex10.cpp
@@ -1,7 +1,24 @@
 #include "Myfunc.h"
 
+constexpr bool FLAG_TEST = true;
+
 int main()
 {
+    if (!FLAG_TEST)
+    {
+        string test;
+        cout << "---input yourself---" << endl;
+        cin >> test;
 
+        evaluate(test);
+    }
+    else
+    {
+        string test = "( 1 + 2 )";
+        cout << "This is Test!" << endl;
+        cout << "input:" << test << endl;
+
+        evaluate(test);
+    }
     return 0;
 }
