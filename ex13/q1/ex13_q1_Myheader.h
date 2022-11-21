91caf9df0b99f74234189dda6ae4d131b26e019d
id-isymhrk <isymhrk0608@gmail.com>
2022-11-21 10:56:27 +0900

    ファイル追加

---
+++ ex13/q1/ex13_q1_Myheader.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+float compute_median(vector<float> array)
+{
+}
---
+++ ex13/q1/test_ex13_1.cpp
@@ -0,0 +1,7 @@
+#include "ex13_q1_Myheader.h"
+
+int main()
+{
+
+    return 0;
+}
