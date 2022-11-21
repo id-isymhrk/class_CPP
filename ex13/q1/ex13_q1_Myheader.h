cea225c8754e24d54105c7fb18f48a08fa59608a
id-isymhrk <isymhrk0608@gmail.com>
2022-11-21 11:13:52 +0900

    q1:完成？

--- ex13/q1/ex13_q1_Myheader.h
+++ ex13/q1/ex13_q1_Myheader.h
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+template <class T>
+void ShowVector(vector<T> array)
+{
+    for (size_t i = 0; i < array.size(); i++)
+    {
+        cout << array[i] << endl;
+    }
+}
+
 float compute_median(vector<float> array)
 {
+    vector<float>::iterator iter = array.begin() + array.size() / 2;
+    nth_element(array.begin(), iter, array.end());
+
+    ShowVector(array);
+
+    return array[array.size() / 2];
 }
--- ex13/q1/test_ex13_1.cpp
+++ ex13/q1/test_ex13_1.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
+    vector<float> test = {0.0, 0.1, 0.4, 0.3, 0.1, 1.0, 0.5, 0.7, 0.4};
+    cout << "---input---" << endl;
+    ShowVector(test);
+    cout << "---output---" << endl;
+    float ans = compute_median(test);
+    cout << "Median:" << ans << endl;
 
     return 0;
 }
